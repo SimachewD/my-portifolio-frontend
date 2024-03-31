2ff6b11b82705490f4f63f65ae9a37f69fefe740
Simachew <simachewdenekewse@gmail.com>
2024-03-31 14:40:45 +0300

    react router edited

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Navigate } from 'react-router-dom';
 
 import Login from './admin/components/Login';
 import Footer from './client/components/Footer';
@@ -19,16 +19,12 @@ import { useAuthContext } from './admin/hooks/useAuthContext';
 
 function App() {
 
-  const path = window.location.pathname;
-
   const { user } = useAuthContext();
 
-  return (
-  <>  
-   <BrowserRouter basename={process.env.PUBLIC_URL}>
-      {path !== '/my-portifolio-frontend' && user ? <NavBar />:null}
-      <Routes>
-        <Route path='/' element={
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/my-portifolio-frontend'>
+        <Route index element={
               <div className="flex flex-col md:grid md:grid-cols-4 md:gap-4 bg-slate-50 ">
                 <div className="sticky shadow-2xl top-0 md:row-span-2 md:col-span-1"><Navbar /></div>
                 <div className="md:ml-4 md:col-span-3">
@@ -40,16 +36,23 @@ function App() {
                 <div className="col-span-3"><Footer /></div>
             </div>}  />
 
+        <Route path='admin' element = { user ? <NavBar />:null}>
+            <Route index element={ user ? <AdminDashboard />:<Login />} />
+            <Route path='login' element = { !user ? <Login />:<Navigate to={'../home'} />} /> 
+            <Route path='home' element={ user ? <AdminDashboard />:<Login />} />
+            <Route path='projects' element={ user ? <Projects />:<Navigate to={'../login'} /> } />
+            <Route path='skills' element={ user ? <AdminSkills />:<Navigate to={'../login'} />} />
+            <Route path='about' element={ user ? <AboutMe />:<Navigate to={'../login'} />} />
+          <Route path='messages' element={ user ? <Messages />:<Navigate to={'../login'} />} />
+        </Route>    
 
-          <Route path='/admin' element={ user ? <AdminDashboard />:<Login />} />
-          <Route path='/admin/projects' element={ user ? <Projects />:<Login />} />
-          <Route path='/admin/skills' element={ user ? <AdminSkills />:<Login />} />
-          <Route path='/admin/about' element={ user ? <AboutMe />:<Login />} />
-          <Route path='/admin/messages' element={ user ? <Messages />:<Login />} />
-      </Routes>
-    </BrowserRouter>
+      </Route>
+    )
+  )
 
-  </>
+
+  return (
+  <RouterProvider router={router} />
   );
 }
 
--- src/admin/components/NavBar.js
+++ src/admin/components/NavBar.js
@@ -15,23 +15,23 @@ const NavBar = () => {
         <nav className="bg-gray-800 p-4 mb-12 text-white">
             <div className="container mx-auto">
                 <div className="flex justify-between">
-                    <Link to="/admin/home" className="text-white font-bold text-lg">Admin</Link>
+                    <Link to="home" className="text-white font-bold text-lg">Admin</Link>
                     {/* Navigation links */}
                     <ul className={`lg:flex lg:items-center lg:w-auto `}>
                         <li>
-                            <NavLink to="/admin/home" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-300 mr-4">Home</NavLink>
+                            <NavLink to="home" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-300 mr-4">Home</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/admin/projects" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-300 mr-4">Projects</NavLink>
+                            <NavLink to="projects" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-300 mr-4">Projects</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/admin/skills" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-300 mr-4">Skills</NavLink>
+                            <NavLink to="skills" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-300 mr-4">Skills</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/admin/about" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-300 mr-4">About Me</NavLink>
+                            <NavLink to="about" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-300 mr-4">About Me</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/admin/messages" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-300">Messages</NavLink>
+                            <NavLink to="messages" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-300">Messages</NavLink>
                         </li>
                         <li>
                             <button onClick={handleClick} className="rounded-lg block mt-4 ml-14 lg:inline-block lg:mt-0 text-red-500 border-red-600 border-2 p-1 mr-2 bg-white hover:text-red-700 hover:font-bold">Log out</button>
